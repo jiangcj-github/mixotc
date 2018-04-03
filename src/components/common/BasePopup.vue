d201912c9e68b649eb33f9de9392437719017d02
Wang Xin <17634029450@163.com>
2018-04-03 18:40:10 +0800

    弹窗外壳

--- src/components/common/BasePopup.vue
+++ src/components/common/BasePopup.vue
@@ -1,15 +1,47 @@
 <template>
-  <div>
-
+  <div class="wrap" v-if="show">
+    <div class="content" :style="{width: `${width}px`, height: `${height}px`}">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    
+    props: {
+      show: {
+        type: Boolean,
+        default: true
+      },
+      width: {
+        type: Number,
+        default: 292
+      },
+      height: {
+        type: Number,
+        default: 94
+      }
+    }
   }
 </script>
 
-<style scoped>
-
+<style scoped lang="stylus">
+  .wrap
+    position fixed
+    top 0
+    left 0
+    width 100%
+    height 100%
+    background rgba(0, 0, 0, 0.4)
+    .content
+      position absolute
+      top 29.17%
+      left 50%
+      transform translateX(-50%)
+      -ms-transform translateX(-50%)	
+      -moz-transform translateX(-50%)
+      -webkit-transform translateX(-50%)
+      -o-transform translateX(-50%)
+      background #FFF
+      border-radius 2px
 </style>
