c76a9af58c697894b92674d525912320e95b4e7e
Wang Xin <17634029450@163.com>
2018-04-09 19:36:34 +0800

    alter

--- src/components/common/Login.vue
+++ src/components/common/Login.vue
@@ -19,7 +19,7 @@
         <img src="/static/images/rules_unchecked.png" alt="" v-else @click="agree = true">
         <p>我已阅读并同意 <a href="">用户协议</a></p>
       </div>
-      <span :class="{'hide-tips':agree,'yhxy-tips':!agree}"><i>!</i>&nbsp&nbsp请勾选用户协议</span>
+      <span :class="{'hide-tips':agree,'yhxy-tips':!agree}"><i>!</i>&nbsp;&nbsp;请勾选用户协议</span>
     </div>
   </div>
 </template>
@@ -58,7 +58,6 @@
         this.type = this.checkAccount(this.account);
       },
       login(){
-        console.log("sdsad");
         this.type = this.checkAccount(this.account);
         this.captcha = this.checkCaptcha(this.code);
         this.agree = this.checkAgree();
@@ -71,7 +70,6 @@
     },
     mounted(){
       this.Bus.$on(this.slideStatus,(status) => {
-        console.log(status);
         this.slideStatus = status
       })
     },
