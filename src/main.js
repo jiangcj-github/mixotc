e4b673ba435e4167c8c5aaa0c728a996f6293fb4
jcj <jiangcj@mixotc.com>
2018-04-25 15:06:10 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import App from './App';
+import App from './App3';
 
 import { DatePicker } from "element-ui";
 import '@/stylus/rest.styl'
