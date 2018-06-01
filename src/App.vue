8000a6648b469ab6af46f9b9e678c2f24af4dab8
Wang Xin <17634029450@163.com>
2018-06-01 20:57:23 +0800

    修复不识别win64位操作系统的bug

--- src/App.vue
+++ src/App.vue
@@ -48,6 +48,7 @@
       }
     },
     beforeCreate(){
+      console.log(detectOS())
       let u = navigator.userAgent;
       let isAndroid = u.indexOf("Android") > -1 || u.indexOf("Adr") > -1; //android终端
       let isiOS = !!u.match(/\(i[^;]+;( U;)? CPU. + Mac OS X/); //ios终端
--- src/js/Os.js
+++ src/js/Os.js
@@ -1,6 +1,6 @@
 function detectOS() {
   let sUserAgent = navigator.userAgent;
-  let isWin = navigator.platform == "Win32" || navigator.platform == "Windows";
+  let isWin = navigator.platform == "Win32" || navigator.platform == "Windows" || navigator.platform == "Win64";
   let isMac =
     navigator.platform == "Mac68K" ||
     navigator.platform == "MacPPC" ||
@@ -12,29 +12,41 @@ function detectOS() {
   let isLinux = String(navigator.platform).indexOf("Linux") > -1;
   if (isLinux) return "Linux";
   if (isWin) {
-    let isWin2K =
-      sUserAgent.indexOf("Windows NT 5.0") > -1 ||
-      sUserAgent.indexOf("Windows 2000") > -1;
+    let isWin2K = sUserAgent
+        .toLowerCase()
+        .indexOf(
+          "windows nt 5.0"
+        ) > -1 || sUserAgent.toLowerCase().indexOf("windows 2000") > -1;
     if (isWin2K) return "Windows 2000";
-    let isWinXP =
-      sUserAgent.indexOf("Windows NT 5.1") > -1 ||
-      sUserAgent.indexOf("Windows XP") > -1;
+    let isWinXP = sUserAgent
+        .toLowerCase()
+        .indexOf(
+          "windows nt 5.1"
+        ) > -1 || sUserAgent.toLowerCase().indexOf("windows XP") > -1;
     if (isWinXP) return "Windows XP";
-    let isWin2003 =
-      sUserAgent.indexOf("Windows NT 5.2") > -1 ||
-      sUserAgent.indexOf("Windows 2003") > -1;
+    let isWin2003 = sUserAgent
+        .toLowerCase()
+        .indexOf(
+          "windows nt 5.2"
+        ) > -1 || sUserAgent.toLowerCase().indexOf("windows 2003") > -1;
     if (isWin2003) return "Windows 2003";
-    let isWinVista =
-      sUserAgent.indexOf("Windows NT 6.0") > -1 ||
-      sUserAgent.indexOf("Windows Vista") > -1;
+    let isWinVista = sUserAgent
+        .toLowerCase()
+        .indexOf(
+          "windows nt 6.0"
+        ) > -1 || sUserAgent.toLowerCase().indexOf("windows vista") > -1;
     if (isWinVista) return "Windows Vista";
-    let isWin7 =
-      sUserAgent.indexOf("Windows NT 6.1") > -1 ||
-      sUserAgent.indexOf("Windows 7") > -1;
+    let isWin7 = sUserAgent
+        .toLowerCase()
+        .indexOf(
+          "windows nt 6.1"
+        ) > -1 || sUserAgent.toLowerCase().indexOf("windows 7") > -1;
     if (isWin7) return "Windows 7";
-    let isWin10 =
-      sUserAgent.indexOf("Windows NT 10") > -1 ||
-      sUserAgent.indexOf("Windows 10") > -1;
+    let isWin10 = sUserAgent
+    .toLowerCase()
+    .indexOf(
+      "windows nt 10"
+    ) > -1 || sUserAgent.toLowerCase().indexOf("windows 10") > -1;
     if (isWin10) return "Windows 10";
   }
   return "otherOs";
